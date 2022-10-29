a8c543bfb1c950f751ff64b98cce4df37b085f3f
Mastermind <adebowaleadam0@gmail.com>
2022-10-29 03:28:55 -0700

    Initial commit

--- GreatKart/settings.py
+++ GreatKart/settings.py
@@ -119,6 +119,8 @@ STATIC_ROOT = BASE_DIR / 'static/'
 STATICFILES_DIRS = [
     'GreatKart/static',
 ]
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
--- GreatKart/urls.py
+++ GreatKart/urls.py
@@ -15,10 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 
 from . import views
+from .settings import MEDIA_ROOT
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
-]
+] + static(settings.MEDIA_URL, document_root=MEDIA_ROOT)
